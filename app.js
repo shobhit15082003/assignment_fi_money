9d7d9459a834fec89725814d64e9d1934ed32e89
shobhit15082003 <shobhit.2201140ec@iiitbh.ac.in>
2025-07-25 19:38:10 +0530

    db connected

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const sequelize=require('./src/config/db');
 
 dotenv.config();
 
@@ -11,6 +12,13 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    try{
+        await sequelize.authenticate();
+        console.log('DB Connected Successfully');
+    }catch(error){
+        console.log('DB Connected Failed');
+        console.log(error);
+    }
   console.log(`Server started on port ${PORT}`);
 });
